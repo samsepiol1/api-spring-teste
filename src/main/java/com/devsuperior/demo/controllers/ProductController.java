558b1d4e9309a099d04a6a2d34e833a0b7162559
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-09-18 02:08:12 +0000

    finalizando api

--- src/main/java/com/devsuperior/demo/controllers/ProductController.java
+++ src/main/java/com/devsuperior/demo/controllers/ProductController.java
@@ -1,22 +1,36 @@
 package com.devsuperior.demo.controllers;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.demo.entities.Departament;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.entities.Product;
 
 @RestController
 @RequestMapping(value = "/products")
 
 public class ProductController{
 
-    public Departament getObjects(){
+    @GetMapping
 
-        Departament d1 = new Departament();
-        d1.setId();
-        d1.setName("Tech");
+    public List<Product> getObjects(){
+        Department d1 = new Department(1L, "Tech");
+        Department d2 = new Department(2L, "Pet");
 
-        return d1;
 
+     
+        Product p1 = new Product(3L, "nome2", null, d1);
+        Product p2 = new Product(4L, "nome", null, d2);
+
+        List<Product> list = Arrays.asList(p1,p2);
+        
+        return list;
+        
+      
+        
 
     }
 
--- src/main/java/com/devsuperior/demo/entities/Department.java
+++ src/main/java/com/devsuperior/demo/entities/Department.java
@@ -1,8 +1,17 @@
 package com.devsuperior.demo.entities;
 
-public class Departament{
+public class Department{
     private Long id;
     private String name;
+
+    public Department(){
+        
+    }
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId(){
         return id;
